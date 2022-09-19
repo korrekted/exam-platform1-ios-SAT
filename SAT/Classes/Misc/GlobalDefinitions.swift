bf998e1bab3c6bd2daf1d12bbac7480c518725c1
akonst17 <akonst17@gmail.com>
2022-09-19 22:27:28 +0300

    интеграция sdk 2.0; новые урлы; build 9

--- SAT/Classes/Misc/GlobalDefinitions.swift
+++ SAT/Classes/Misc/GlobalDefinitions.swift
@@ -6,21 +6,24 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams.korrekted.com"
+    static let domainUrl = "https://exams3.mastrapi.com"
     static let apiKey = "udgKkp1cMgglBQXm"
-
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "Dk7zPgVXymInFKq5"
 
     static let applicationTag = "EXAM_SAT"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "Dk7zPgVXymInFKq5"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "hYD19U3j3FSk3hat"
 
     static let appStoreUrl = "https://apps.apple.com/app/sat-exam-prep-and-practice/id1565837095"
     static let appleAppID = "1565837095"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
